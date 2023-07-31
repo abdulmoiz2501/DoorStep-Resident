1a1c8e00b1b54098a07e35d0a25bfa225d73eea7
Abdul Moiz <82509886+abdulmoiz2501@users.noreply.github.com>
2023-08-01 02:13:06 +0500

    Signup page UI done only

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -31,6 +31,7 @@ class _LoginPageState extends State<LoginPage> {
               Icon(
                 Icons.lock,
                 size: 100,
+                color: kAccentColor,
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.1,
@@ -74,7 +75,8 @@ class _LoginPageState extends State<LoginPage> {
 
               //forgot pass
               Padding(
-                padding:  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.065),
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.065),
                 child: Row(
                   children: [
                     Text(
@@ -90,10 +92,57 @@ class _LoginPageState extends State<LoginPage> {
                 height: MediaQuery.of(context).size.height * 0.024,
               ),
               //signin
-              SignInButton(onTap: signUserIn,),
+              SignInButton(
+                onTap: signUserIn,
+              ),
               //continue with
 
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.044,
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.025),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.2,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal:
+                              MediaQuery.of(context).size.width * 0.025),
+                      child: Text(
+                        'Or continue with ',
+                        style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height * 0.019,
+                          color: kTextColor,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.2,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
               //google signin
+              Row(
+                children: [
+                  Image.asset(
+                    'lib/assets/images/google.png',
+                    height: MediaQuery.of(context).size.height * 0.065,
+                  ),
+                ],
+              ),
 
               //not a user?
             ],
